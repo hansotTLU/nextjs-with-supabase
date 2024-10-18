0ba508ee8d6ed9592aaffa70c4563ac2a0624227
Hans Oskar Trolla <146342767+hansotTLU@users.noreply.github.com>
2024-10-18 16:26:02 +0300

    feat: add Jest testing
    testing TODO header and counter component

---
+++ __tests__/todo.test.jsx
@@ -0,0 +1,19 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Todo from "../app/todo/page";
+
+jest.mock("../app/todo/page", () => () => (
+  <div>
+    <h1>To Do lists:</h1>
+  </div>
+));
+
+describe("Todo", () => {
+  it("renders a heading", () => {
+    render(<Todo />);
+
+    const heading = screen.getByRole("heading", { level: 1 });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -4,6 +4,7 @@ import ServerTodoComponent from "@/components/todo/ServerTodoComponent";
 export default async function Page() {
   return (
     <>
+      <h1>To Do lists:</h1>
       <ServerTodoComponent />
       <ClientTodoComponent />
     </>
